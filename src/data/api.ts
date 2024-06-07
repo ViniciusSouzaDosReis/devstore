15c0840110e915f7cd4d684bf5f2ea2990a300a9
Vinicius Reis <vinireis010305@gmail.com>
2024-06-07 12:48:06 -0300

    feat: :sparkles: create fetch API wrapper

---
+++ src/data/api.ts
@@ -0,0 +1,8 @@
+import { env } from '@/app/env'
+
+export function api(path: string, init?: RequestInit) {
+  const baseURL = env.NEXT_PUBLIC_BASE_URL
+  const url = new URL(path, baseURL)
+
+  return fetch(url, init)
+}
