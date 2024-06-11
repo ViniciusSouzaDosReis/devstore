cd136311c7765ae01a228582752312e44c3758ad
Vinicius Reis <vinireis010305@gmail.com>
2024-06-11 17:42:03 -0300

    feat: :sparkles: add delay in req when is dev mode

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,9 +1,17 @@
 import { env } from '@/app/env'
 
-export function api(path: string, init?: RequestInit) {
+function withDelay<T>(fn: () => Promise<T>, delay: number = 1000): Promise<T> {
+  if (process.env.NODE_ENV === 'development') {
+    return new Promise((resolve) => setTimeout(() => resolve(fn()), delay))
+  } else {
+    return fn()
+  }
+}
+
+export async function api(path: string, init?: RequestInit) {
   const baseURL = env.NEXT_PUBLIC_BASE_URL
   const prefixApi = '/api'
   const url = new URL(`${prefixApi}/${path}`, baseURL)
 
-  return fetch(url, init)
+  return withDelay(() => fetch(url, init), 1000)
 }
