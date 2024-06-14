756640d5616d693133a49ecbfbee56d5dacd16c3
Vinicius Reis <vinireis010305@gmail.com>
2024-06-14 18:43:11 -0300

    feat: :sparkles: create cart-context

---
+++ src/contexts/cart-context.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { ReactNode, createContext, useContext, useState } from 'react'
+
+interface CartItem {
+  productId: string
+  quantity: number
+}
+
+interface CartContextType {
+  itens: CartItem[]
+  addToCart: (productId: string) => void
+}
+
+const CartContext = createContext({} as CartContextType)
+
+export function CartProvider({ children }: { children: ReactNode }) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+  function addToCart(productId: string) {
+    setCartItems((state) => {
+      const haveThisItemInCart = state.some(
+        (item) => item.productId === productId,
+      )
+
+      if (haveThisItemInCart) {
+        return state.map((item) => {
+          if (item.productId === productId) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            }
+          } else {
+            return item
+          }
+        })
+      } else {
+        return [...state, { productId, quantity: 1 }]
+      }
+    })
+  }
+
+  return (
+    <CartContext.Provider value={{ itens: cartItems, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export const useCart = () => useContext(CartContext)
