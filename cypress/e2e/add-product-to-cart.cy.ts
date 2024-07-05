d7f83ee715ae2e235953306f35ea88e16db04f37
Vinicius Reis <vinireis010305@gmail.com>
2024-07-05 19:15:16 -0300

    feat: :sparkles: creating tests

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,38 @@
+describe('add product to cart', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('should be able to navigate to product page and add it to the cart', () => {
+    // find href start with '/product'
+    cy.get('a[href^="/product"]').first().click()
+    cy.location('href').should('include', '/product')
+
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should not be able to add the same product to the cart', () => {
+    // find href start with '/product'
+    cy.get('a[href^="/product"]').first().click()
+    cy.location('href').should('include', '/product')
+
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should be able to search for a product and add it to the cart', () => {
+    cy.get('input[name="q"]').type('Moletom').parent('form').submit()
+    cy.location('href').should('include', '/search')
+
+    cy.get('a[href^="/product"]').first().click()
+    cy.location('href').should('include', '/product')
+
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+})
---
+++ cypress/e2e/search-product.cy.ts
@@ -0,0 +1,24 @@
+describe('search product', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('should be able to navigate to product page and add it to the cart', () => {
+    cy.get('input[name="q"]').type('Moletom').parent('form').submit()
+
+    cy.location('href').should('include', '/search')
+    cy.location('search').should('include', 'q=Moletom')
+
+    cy.get('a[href^="/product"]').should('exist')
+  })
+
+  it('should not be able navigate to search page without query', () => {
+    cy.on('uncaught:exception', () => {
+      return false
+    })
+
+    cy.visit('/search')
+
+    cy.location('pathname').should('equal', '/')
+  })
+})
