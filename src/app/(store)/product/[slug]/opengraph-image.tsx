abeb8273a7383b99319be34049131cf42be9d280
Vinicius Reis <vinireis010305@gmail.com>
2024-07-05 15:08:47 -0300

    feat: :sparkles: add opengraph to product page

---
+++ src/app/(store)/product/[slug]/opengraph-image.tsx
@@ -0,0 +1,57 @@
+import { api } from '@/data/api'
+import { Product } from '@/data/types/products'
+import { ImageResponse } from 'next/server'
+import colors from 'tailwindcss/colors'
+
+export const runtime = 'edge'
+
+export const alt = 'About Acme'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
+
+  const products = await response.json()
+
+  return products
+}
+
+export default async function Image({
+  params,
+}: {
+  params: {
+    slug: string
+  }
+}) {
+  const product = await getProduct(params.slug)
+
+  const productURL = new URL(product.image, process.env.APP_URL).toString()
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: colors.zinc[950],
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <img src={productURL} alt="" />
+      </div>
+    ),
+    {
+      ...size,
+    },
+  )
+}
